96b7541e62aba9edf22b669882620c5d147c74cd
arupdeb <arup.goku.deb@gmail.com>
2022-04-30 23:51:52 +0530

    Added Array package

---
+++ arrays/dynamic_array.go
@@ -0,0 +1,18 @@
+//static array
+//len, cap
+//create new array
+//size, isEmpty
+//get(index) return element //boubds check
+//set(index,element) //bounds check
+//clear, add
+//removeAt(index), remove(object)
+//indexof
+
+package arrays
+
+type Array struct {
+	elements []interface{}
+	size int
+	capacity int
+}
+
