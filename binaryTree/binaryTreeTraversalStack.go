36e82e91b86408e65a28edae01d71d23262adef2
arupdeb <arup.goku.deb@gmail.com>
2022-05-21 16:07:10 +0530

    fixed pre order traversal method 2: and inorder traversal

--- binaryTree/binaryTreeTraversalStack.go
+++ binaryTree/binaryTreeTraversalStack.go
@@ -13,27 +13,28 @@ func (t *tree) InorderTraversalStack(root *Node) {
 		return
 	}
 	s := stacks.CreateNewStack()
+	var curr *Node = root
 
-	for i := root; i != nil || !s.IsEmpty(); {
+	for curr != nil || !s.IsEmpty() {
 
-		for j := i; j != nil; {
-			s.Push(j)
-			j = j.left
+		for curr != nil {
+			s.Push(curr)
+			curr = curr.left
 		}
-		//j is nil after visiting all the left nodes
-
+		//curr is nil after visiting all the left nodes
 		// get the topmost entry and pop the value i.e visisted
 		top, err := s.Pop()
 		if err != nil {
 			log.Println(err.Error())
+			continue
 		}
 
 		// store the data to i for visiting
-		i = top.(*Node)
-		log.Println(i.data)
-
+		log.Println(top.(*Node).data)
+		curr = top.(*Node)
 		// visit right subtree
-		i = i.right
+			curr = curr.right
+		
 	}
 }
 
@@ -48,14 +49,14 @@ func (t *tree) PreOrderTraversalStack(root *Node) {
 
 	for !s.IsEmpty() {
 
-		headElement, err := s.Peek()
+		//pop the first element if not empty
+		headElement, err := s.Pop()
 		if err != nil {
 			log.Println(err.Error())
 			break
 		}
 		data := headElement.(*Node)
-		log.Println(data.data)  // Print the root node
-		s.Pop()
+		log.Println(data.data) // Print the root node
 
 		if data.right != nil {
 			s.Push(data.right)
@@ -73,58 +74,64 @@ func (t *tree) PreOrderTraversalStack2(root *Node) {
 	}
 
 	s := stacks.CreateNewStack()
-	s.Push(root)
+	var curr *Node = root //initalize current variable
 
-	for !s.IsEmpty() {
+	for !s.IsEmpty() || curr != nil {
 
-		headElement, err := s.Peek()
-		if err != nil {
-			log.Println(err.Error())
-			break
+		//Print the left/root node first and push the right child to the stack
+		//update the current node to the left node
+		for curr != nil {
+			log.Println(curr.data)
+
+			if curr.right != nil {
+				s.Push(curr.right)
+			}
+			curr = curr.left
 		}
-		data := headElement.(*Node)
-		log.Println(data.data)  // Print the root node
-		s.Pop()
-
-		if data.right != nil {
-			s.Push(data.right)
-		}
-		if data.left != nil {
-			s.Push(data.left)
+		//here curr will be nil after the loop completes
+		//take out the topmost from stack i.e right child of the node printed above
+		//set current to the right child / popped element 
+		if !s.IsEmpty() {
+			val, err := s.Pop()
+			if err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			curr = val.(*Node)
 		}
 	}
 }
 
 // PostOrderTraversalStack : uses stack to traverse the binary tree using post-order traversal- DFS
-func (t *tree) PostOrderTraversalStack(root *Node) {
-	s := stacks.CreateNewStack()
+// func (t *tree) PostOrderTraversalStack(root *Node) {
+// 	s := stacks.CreateNewStack()
 
-	for i := root; i != nil || !s.IsEmpty(); {
+// 	for i := root; i != nil || !s.IsEmpty(); {
 
-		for j := i; j != nil; {
-			s.Push(j)
-			j = j.left
-		}
-		//j is nil after visiting all the left nodes
+// 		for j := i; j != nil; {
+// 			s.Push(j)
+// 			j = j.left
+// 		}
+// 		//j is nil after visiting all the left nodes
 
-		// get the topmost entry and peek the value
-		top, err := s.Peek()
-		if err != nil {
-			log.Println(err.Error())
-		}
+// 		// get the topmost entry and peek the value
+// 		top, err := s.Peek()
+// 		if err != nil {
+// 			log.Println(err.Error())
+// 		}
 
-		// store the data to i for visiting
-		i = top.(*Node)
+// 		// store the data to i for visiting
+// 		i = top.(*Node)
 
-		//Print the right node value
-		if i.right != nil {
-			log.Println("i.right.data : ", i.right.data)
-		}
-		//Print the Root node value
-		log.Println("i.data : ", i.data)
-		s.Pop() // visited
-		// visit right subtree
-		i = i.right
+// 		//Print the right node value
+// 		if i.right != nil {
+// 			log.Println("i.right.data : ", i.right.data)
+// 		}
+// 		//Print the Root node value
+// 		log.Println("i.data : ", i.data)
+// 		s.Pop() // visited
+// 		// visit right subtree
+// 		i = i.right
 
-	}
-}
+// 	}
+// }
--- binaryTree/binaryTreeTraversalStack_test.go
+++ binaryTree/binaryTreeTraversalStack_test.go
@@ -46,6 +46,28 @@ func Test_tree_InorderTraversalStack(t *testing.T) {
 				return t
 			},
 		},
+		{
+			name: "Traverse only root node and left node and right node and more",
+			beforeTest: func() *tree {
+				t := CreateNewTree(123)
+				t.root.left = &Node{
+					data:  456,
+					left:  nil,
+					right: nil,
+				}
+				t.root.right = &Node{
+					data:  428,
+					left:  nil,
+					right: nil,
+				}
+				t.root.right.right = &Node{
+					data:  444,
+					left:  nil,
+					right: nil,
+				}
+				return t
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -94,6 +116,11 @@ func Test_tree_PreOrderTraversalStack(t *testing.T) {
 					left:  nil,
 					right: nil,
 				}
+				t.root.right.right = &Node{
+					data:  612,
+					left:  nil,
+					right: nil,
+				}
 				return t
 			},
 		},
@@ -102,6 +129,62 @@ func Test_tree_PreOrderTraversalStack(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			tr := tt.beforeTest()
 			tr.PreOrderTraversalStack(tr.root)
+		})
+	}
+}
+
+func Test_tree_PreOrderTraversalStack2(t *testing.T) {
+
+	tests := []struct {
+		name       string
+		beforeTest func() *tree
+	}{
+		{
+			name: "Traverse only root node",
+			beforeTest: func() *tree {
+				t := CreateNewTree(123)
+				return t
+			},
+		},
+		{
+			name: "Traverse only root node and left node",
+			beforeTest: func() *tree {
+				t := CreateNewTree(123)
+				t.root.left = &Node{
+					data:  456,
+					left:  nil,
+					right: nil,
+				}
+				return t
+			},
+		},
+		{
+			name: "Traverse only root node and left node and right node",
+			beforeTest: func() *tree {
+				t := CreateNewTree(123)
+				t.root.left = &Node{
+					data:  456,
+					left:  nil,
+					right: nil,
+				}
+				t.root.right = &Node{
+					data:  428,
+					left:  nil,
+					right: nil,
+				}
+				t.root.right.right = &Node{
+					data:  512,
+					left:  nil,
+					right: nil,
+				}
+				return t
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tr := tt.beforeTest()
+			tr.PreOrderTraversalStack2(tr.root)
 		})
 	}
 }
